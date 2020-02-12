c54b1b2237fc86632dd62940fac638ebf54cd7f7
Kamo <52213066+kamogelo-r@users.noreply.github.com>
2020-02-12 13:43:06 +0200

    Upload project files

---
+++ Factory/Admin.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Factory
+{
+    public class Admin
+    {
+        public readonly ICustomer cust; //DI: assigns objects; makes testing easier; less code for modifications
+
+        /// <summary>
+        /// custIOC passed from ninject
+        /// </summary>
+        /// <param name="custIOC"></param>
+        public Admin(ICustomer custIOC) //IOC: binds an interface to its class and creates objects at runtime (ninject = IOC)
+        {
+            cust = custIOC;
+        }
+
+        public void GetCustomerDetails()
+        {
+            cust.GetDetails();
+        }
+    }
+}
